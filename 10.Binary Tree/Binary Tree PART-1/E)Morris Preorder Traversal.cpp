b56d2d3665e66fb05e80f04eefed432ff183b5ab
Vanshit <vanshitagrawal8079@gmail.com>
2024-06-10 15:26:47 +0530

    Morris Preorder Traversal

---
+++ 10.Binary Tree/Binary Tree PART-1/E)Morris Preorder Traversal.cpp
@@ -0,0 +1,50 @@
+//*****https://leetcode.com/problems/binary-tree-preorder-traversal/description/*****
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+//T.C: O(n) + O(n);
+//S.C: O(1);
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr -> left == NULL){
+                ans.push_back(curr -> val);
+                curr = curr -> right;
+            }
+            else{
+                TreeNode* prev = curr -> left;
+                while(prev -> right != NULL && prev -> right != curr){
+                    prev = prev -> right;
+                }
+
+                if(prev -> right == NULL){
+                    prev -> right = curr;
+                    ans.push_back(curr -> val);
+                    curr = curr -> left;
+                }
+
+                else{
+                    prev -> right = NULL;
+                    // ans.push_back(curr -> val);
+                    curr = curr -> right;
+                }
+            }
+        }
+        return ans;
+    }
+};
