7f62eeed5e5e2dcbc33f56f3d9dcf0043c32b5ac
Vanshit <vanshitagrawal8079@gmail.com>
2024-06-12 16:51:24 +0530

    Check if two trees are identical or not

--- 10.Binary Tree/Binary Tree PART-2/F)Check if two trees are identical or not.cpp
+++ 10.Binary Tree/Binary Tree PART-2/F)Check if two trees are identical or not.cpp
@@ -14,6 +14,7 @@
  */
 
 
+//*****Using Recursion*****
 //T.C: O(n);
 //S.C: O(n);
 class Solution {
@@ -27,5 +28,41 @@ public:
         bool right = isSameTree(p -> right, q -> right);
 
         return left & right;
+    }
+};
+
+
+//*****Using Level Order Traversal*****
+/*Here we need to push NULL node also in the queue 
+  so that we can compare even at the NULL node that
+  if on one tree we are at the NULL node than on 
+  other tree also we should be present at the NULL
+  node only. */
+//T.C: O(n);
+//S.C: O(n);
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> Q;
+        if(p == NULL || q == NULL) return p==q;               //If both roots are null
+        Q.push(p); Q.push(q);                                 //Push both the roots in the queue      
+                    
+        while(!Q.empty())  {
+            TreeNode* left = Q.front(); Q.pop();              //Store one node in left and pop
+            TreeNode* right = Q.front(); Q.pop();             //Store other in right and pop
+
+            if(left == NULL && right == NULL) continue;       //If both nodes are null -> continue
+            if(left == NULL || right == NULL) return false;   //If one of them is null, simply return false
+            if(left->val != right->val) return false;         //If they are not equal, return false
+            
+            //Push left childs of both nodes
+            Q.push(left->left);                         
+            Q.push(right->left);
+
+            //Push right child of both nodes
+            Q.push(left->right);
+            Q.push(right->right);
+        }
+        return true;  
     }
 };
