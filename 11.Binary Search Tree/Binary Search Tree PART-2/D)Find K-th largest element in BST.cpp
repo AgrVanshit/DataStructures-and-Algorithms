c27b09f60d970b94d585d24234ec4fb128710995
Vanshit <vanshitagrawal8079@gmail.com>
2024-06-14 18:18:57 +0530

    Find K-th largest element in BST

---
+++ 11.Binary Search Tree/Binary Search Tree PART-2/D)Find K-th largest element in BST.cpp
@@ -0,0 +1,5 @@
+/*For kth largest we can do a reverse inorder kind 
+  of thing: RIGHT ROOT LEFT with the logic we have 
+  used for findig k-th smallest in BST, We can even 
+  find (n-k+1)th smallest for kth largest */
+
