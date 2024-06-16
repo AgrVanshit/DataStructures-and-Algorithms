29f7eefc8ecaf69330b440ed184de1474e569236
Vanshit <vanshitagrawal8079@gmail.com>
2024-06-16 11:40:18 +0530

    Detect A cycle in a Directed Graph using BFS

--- 12.Graph/Graph/G)Detect A cycle in a Directed Graph using BFS.cpp
+++ 12.Graph/Graph/G)Detect A cycle in a Directed Graph using BFS.cpp
@@ -1,1 +1,45 @@
 //*****https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1*****
+
+
+//T.C: O(n + E) + O(n) + O(n) + O(E);
+//S.C: 2*O(n);
+class Solution {
+public:
+	vector<int> topo(int N, vector<int> adj[]) {
+        queue<int> q; 
+        vector<int> indegree(N, 0); 
+
+	    for(int i = 0; i < N; i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0; i < N; i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+        
+        int cnt = 0;
+
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop();
+
+            cnt++;
+
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+
+        /*(cnt == N) means topo sort have all N nodes, means topo sort is 
+          possible hence, graph is Directed and Acyclic. */
+	    if(cnt == N) return false;
+        return true;
+	}
+};
