0bf0da889023d024a72cfb1d5458a1deab3a8c74
Vanshit <vanshitagrawal8079@gmail.com>
2024-06-19 17:38:09 +0530

    MST using Prim's Algo

--- 12.Graph/Graph PART-2/E)MST using Prim's Algo.cpp
+++ 12.Graph/Graph PART-2/E)MST using Prim's Algo.cpp
@@ -9,8 +9,7 @@
 class Solution
 {
 	public:
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
+    int spanningTree(int V, vector<vector<int>> adj[]){
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
 		vector<int> vis(V, 0);
@@ -18,21 +17,21 @@ class Solution
 		// {wt, node}
 		pq.push({0, 0});
 		int sum = 0;
-		while (!pq.empty()) {
+		while (!pq.empty()){
 			auto it = pq.top();
 			pq.pop();
 			int node = it.second;
 			int wt = it.first;
 
-			if (vis[node] == 1) continue;
+			if(vis[node] == 1) continue;
 			
 			// add it to the sum
 			vis[node] = 1;
 			sum += wt;
-			for (auto it : adj[node]) {
+			for(auto it : adj[node]){
 				int adjNode = it[0];
 				int edW = it[1];
-				if (!vis[adjNode]) {
+				if(!vis[adjNode]){
 					pq.push({edW, adjNode});
 				}
 			}
