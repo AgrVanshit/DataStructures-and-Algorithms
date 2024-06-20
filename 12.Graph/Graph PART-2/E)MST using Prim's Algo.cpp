a8d287f146953f3a926b7204f3a28690c8ebbdf6
Vanshit <vanshitagrawal8079@gmail.com>
2024-06-20 12:35:48 +0530

    MST using Prim's Algo

--- 12.Graph/Graph PART-2/E)MST using Prim's Algo.cpp
+++ 12.Graph/Graph PART-2/E)MST using Prim's Algo.cpp
@@ -12,31 +12,31 @@ public:
     int spanningTree(int V, vector<vector<int>> adj[]){
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-	vector<int> vis(V, 0);
-		
-	// {wt, node}
-	pq.push({0, 0});
-	int sum = 0;
-	while(!pq.empty()){
-	      auto it = pq.top();
-	      pq.pop();
-	      int node = it.second;
-	      int wt = it.first;
+		vector<int> vis(V, 0);
+			
+		// {wt, node}
+		pq.push({0, 0});
+		int sum = 0;
+		while(!pq.empty()){
+			auto it = pq.top();
+			pq.pop();
+			int node = it.second;
+			int wt = it.first;
 
-	      if(vis[node] == 1) continue;
-	
-	      // add it to the sum
-              vis[node] = 1;
-	      sum += wt;
-	      for(auto it : adj[node]){
-	          int adjNode = it[0];
-	          int edW = it[1];
-                  if(!vis[adjNode]){
-		      pq.push({edW, adjNode});
-	          }
-	      }
-	}
-	return sum;
+			if(vis[node] == 1) continue;
+		
+			// add it to the sum
+				vis[node] = 1;
+			sum += wt;
+			for(auto it : adj[node]){
+				int adjNode = it[0];
+				int edW = it[1];
+					if(!vis[adjNode]){
+				pq.push({edW, adjNode});
+				}
+			}
+		}
+		return sum;
     }
 };
 
@@ -51,34 +51,34 @@ public:
     {
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>> pq;
 
-	vector<int> vis(V, 0);
-        vector<pair<int, int>> mst;
-        // {parent, node}
-		
-	// {wt, {node, parent}}
-	pq.push({0, {0, -1}});
-	int sum = 0;
-	while(!pq.empty()) {
-	      auto it = pq.top();
-	      pq.pop();
-	      int node = it.second.first;
-	      int parent = it.second.second;
-	      int wt = it.first;
+		vector<int> vis(V, 0);
+			vector<pair<int, int>> mst;
+			// {parent, node}
+			
+		// {wt, {node, parent}}
+		pq.push({0, {0, -1}});
+		int sum = 0;
+		while(!pq.empty()) {
+			auto it = pq.top();
+			pq.pop();
+			int node = it.second.first;
+			int parent = it.second.second;
+			int wt = it.first;
 
-	      if (vis[node] == 1) continue;
-		
-	      // add it to the mst
-	      if(parent != -1) mst.push_back({parent, node});
-	      vis[node] = 1;
-	      sum += wt;
-	      for(auto it : adj[node]) {
-		  int adjNode = it[0];
-		  int edW = it[1];
-		  if (!vis[adjNode]) {
-		      pq.push({edW, {adjNode, node}});
-	          }
-	      }
-	}
-	return mst;
+			if (vis[node] == 1) continue;
+			
+			// add it to the mst
+			if(parent != -1) mst.push_back({parent, node});
+			vis[node] = 1;
+			sum += wt;
+			for(auto it : adj[node]) {
+			int adjNode = it[0];
+			int edW = it[1];
+			if (!vis[adjNode]) {
+				pq.push({edW, {adjNode, node}});
+				}
+			}
+		}
+		return mst;
     }
 };
