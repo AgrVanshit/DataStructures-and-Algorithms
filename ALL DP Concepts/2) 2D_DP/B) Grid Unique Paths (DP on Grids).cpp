83ffa86c1fbde35c5ce529e2031af61e82aefe3f
Vanshit <vanshitagrawal8079@gmail.com>
2024-07-15 14:56:56 +0530

    Grid Unique Paths : DP on Grids

--- ALL DP Concepts/2) 2D_DP/B) Grid Unique Paths (DP on Grids).cpp
+++ ALL DP Concepts/2) 2D_DP/B) Grid Unique Paths (DP on Grids).cpp
@@ -35,14 +35,14 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m, vector<int>(n,0));
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(i == 0 && j == 0) dp[i][j] = 1;//base case it is
                 else{
                     int up = 0;
                     int left = 0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
 
                     dp[i][j] = up + left;
                 }    
@@ -57,21 +57,21 @@ public:
 //Approach: 3
 //Space Optimization:
 //T.C: O(m*n);
-//S.C: O(m);
+//S.C: O(n);
 class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<int> prev(n, 0);
 
-        for(int i=0; i<m; i++){
+        for(int i = 0; i < m; i++){
             vector<int> curr(n, 0); 
-            for(int j=0; j<n; j++){
+            for(int j = 0; j < n; j++){
                 if(i == 0 && j == 0) curr[j] = 1;//base case it is
                 else{
                     int up = 0;
                     int left = 0;
-                    if(i>0) up = prev[j];
-                    if(j>0) left = curr[j-1];
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = curr[j-1];
 
                     curr[j] = up + left;
                 }    
@@ -79,7 +79,7 @@ public:
 
             prev = curr;
         }
-        
+
         return prev[n-1];
     }
 };
