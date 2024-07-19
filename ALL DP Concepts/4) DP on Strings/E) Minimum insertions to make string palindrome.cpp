7cf88d8e42b48bb1a40d4da366220e46b1654353
Vanshit <vanshitagrawal8079@gmail.com>
2024-07-19 23:35:14 +0530

    Minimum insertions to make string palindrome

--- ALL DP Concepts/4) DP on Strings/E) Minimum insertions to make string palindrome.cpp
+++ ALL DP Concepts/4) DP on Strings/E) Minimum insertions to make string palindrome.cpp
@@ -10,12 +10,7 @@ public:
     int n = text1.size();
     int m = text2.size();
 
-    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-
-    for(int j = 0; j <= m; j++) dp[0][j] = 0;
-    for(int i = 0; i <= n; i++) dp[i][0] = 0;
-    /*No need of above 2 loops, we can just declare all elements of 
-      dp as 0 and hence remove above two loops*/
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
