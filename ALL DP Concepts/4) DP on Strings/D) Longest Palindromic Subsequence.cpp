dd892310490bde48285c8f8a5edf9f262d47fd26
Vanshit <vanshitagrawal8079@gmail.com>
2024-07-19 21:57:39 +0530

    Longest Palindromic Subsequence

---
+++ ALL DP Concepts/4) DP on Strings/D) Longest Palindromic Subsequence.cpp
@@ -0,0 +1,64 @@
+//*****https://leetcode.com/problems/longest-palindromic-subsequence/description/*****
+
+
+//****Tabulation****
+//T.C: O(n * n) + O(n);
+//S.C: O(n * n);
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+    int n = text1.size();
+    int m = text2.size();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+    for(int j = 0; j<=m; j++) dp[0][j] = 0;
+    for(int i = 0; i<=n; i++) dp[i][0] = 0;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    
+    return dp[n][m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+
+        return longestCommonSubsequence(s, t);
+    }
+};
+
+
+//****2 Array Space Optimization****
+//T.C: O(n * n) + O(n);
+//S.C: O(n);
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(text1[i-1] == text2[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j], curr[j-1]);
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+
+        return longestCommonSubsequence(s, t);
+    }
+};
