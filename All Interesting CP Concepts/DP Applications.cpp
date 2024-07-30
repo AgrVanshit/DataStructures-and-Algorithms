ec425657f541127c3b3cca86d557ac78f225ddeb
Vanshit <vanshitagrawal8079@gmail.com>
2024-07-30 11:51:32 +0530

    CP

---
+++ All Interesting CP Concepts/DP Applications.cpp
@@ -0,0 +1,9 @@
+/*
+DP With Binary Search:
+//https://leetcode.com/problems/maximum-total-damage-with-spell-casting/description/
+*/
+
+/*
+Handling Char diff in alphabets:
+//https://leetcode.com/problems/can-convert-string-in-k-moves/description/
+*/
---
+++ All Interesting CP Concepts/Graph concepts Application.cpp
@@ -0,0 +1,5 @@
+/*
+DJIKSTRA'S ALGORITHM:
+//https://leetcode.com/problems/minimum-cost-to-convert-string-i/description/
+//https://leetcode.com/problems/second-minimum-time-to-reach-destination/description/
+*/
