2f0c23428b0a0b86b2aa6adccbf29bcef26f4a9a
Vanshit <vanshitagrawal8079@gmail.com>
2024-09-03 21:17:16 +0530

    Notes

---
+++ CSES/DP/A) Dice Combinations.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    
+}
+
+int main() {
+    int t;
+    t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
